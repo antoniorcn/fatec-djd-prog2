76c0cdd93615f511fd3d4124f04e258e9874173e
Antonio Carvalho <antoniorcn@hotmail.com>
2018-09-03 11:46:25 -0300

    Jogo de dados

---
+++ 2018-2S/manha/aula5/jogo_dados.py
@@ -0,0 +1,19 @@
+import random
+r = "S"
+print("Jogo de dados")
+vida = 3
+while r == "S":
+    n = int(input("Escolha um numero entre 1 e 6"))
+    a = random.randint(1, 6)
+    if n == a:
+        print("Ganhou")
+        continue
+    else:
+        print("Perdeu")
+        vida = vida - 1
+        if vida <= 0:
+            break
+    r = input("Deseja jogar de novo (S/N) ?")
+else:
+    print("Parabéns você desistiu enquanto ainda tinha vidas")
+print("Fim do programa")
