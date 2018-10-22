fb06a6bd32ab93286acbf942b765df5459c1da25
Antonio Carvalho <antoniorcn@hotmail.com>
2018-10-22 10:48:53 -0300

    Manha aula11 parte 2

--- 2018-2S/manha/aula11/dicionario_game_teste.py
+++ 2018-2S/manha/aula11/dicionario_game_teste.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import QUIT, KEYDOWN, KEYUP, K_d, K_a, K_w, K_s, Rect
 
 
@@ -20,6 +21,51 @@ def rect_colide( rectA, rectB ):
     return collideX and collideY
 
 
+def calcula_velocidade( obj ):
+    chegou = False
+    if obj['x'] == obj['destinoX']:
+        obj['velX'] = 0
+        chegou = True
+    elif obj['x'] > obj['destinoX']:
+        obj['velX'] = -1
+    else:
+        obj['velX'] = 1
+
+    if obj['y'] == obj['destinoY']:
+        obj['velY'] = 0
+        chegou = chegou and True
+    elif obj['y'] > obj['destinoY']:
+        obj['velY'] = -1
+    else:
+        obj['velY'] = 1
+
+    return chegou
+
+def movimenta(obj):
+    obj['x'] = obj['x'] + obj['velX']
+    obj['y'] = obj['y'] + obj['velY']
+
+
+def pintar( scr, obj ):
+    scr.blit(obj['imagem'], (obj['x'], obj['y']))
+
+
+def teste_colisao( obj1, obj2 ):
+    r1 = obj1['imagem'].get_rect()
+    r1.x = obj1['x']
+    r1.y = obj1['y']
+    r2 = obj2['imagem'].get_rect()
+    r2.x = obj2['x']
+    r2.y = obj2['y']
+
+    if rect_colide(r1, r2):
+        return True
+    else:
+        return False
+
+
+ciclos = 0
+
 tela = pygame.display.set_mode((800, 600), 0, 32)
 imagem1 = pygame.image.load("./images/gato.png").convert_alpha()
 imagem2 = pygame.image.load("./images/enemy.png").convert_alpha()
@@ -27,33 +73,31 @@ imagem3 = pygame.image.load("./images/explosion.png").convert_alpha()
 
 rinimigo = imagem2.get_rect()
 img_explod = pygame.transform.scale(imagem3, rinimigo.size)
-x = 100
-y = 100
-velX = 0
-velY = 0
 explode = False
+
+gato = {'x': 100, 'y': 100, 'velX': 0, 'velY': 0, 'imagem': imagem1}
+inimigo = {'x': 400, 'y': 300, 'velX': 1,
+           'velY': -1, 'imagem': imagem2, 'destinoX': 10, 'destinoY': 10}
+
 while True:
     # Calcular Regras
-    x = x + velX
-    y = y + velY
-    r1 = imagem1.get_rect()
-    r1.x = x
-    r1.y = y
-    r2 = imagem2.get_rect()
-    r2.x = 400
-    r2.y = 300
+    ciclos += 1
+    movimenta(gato)
+    chegou = calcula_velocidade(inimigo)
+    movimenta(inimigo)
 
-    if rect_colide(r1, r2):
-        explode = True
-    else:
-        explode = False
+    if ciclos % 100 == 0 and chegou:
+        # inimigo['destinoX'] = random.randint(50, 750)
+        # inimigo['destinoY'] = random.randint(50, 550)
+        inimigo['destinoX'] = gato['x']
+        inimigo['destinoY'] = gato['y']
 
     # Desenhar na tela
     tela.fill((0, 0, 0))
-    tela.blit(imagem1, (x, y))
-    tela.blit(imagem2, (400, 300))
-    if explode == True:
-        tela.blit(img_explod, (400, 300))
+    pintar(tela, gato)
+    pintar(tela, inimigo)
+    if teste_colisao(gato, inimigo):
+        tela.blit(img_explod, (inimigo['x'], inimigo['y']))
     pygame.draw.circle(tela, (0, 255, 0), (600, 100), 50, 0)
     pygame.display.update()
 
@@ -62,19 +106,19 @@ while True:
             exit()
         elif e.type == KEYDOWN:
             if e.key == K_d:
-                velX = 1
+                gato['velX'] = 1
             elif e.key == K_a:
-                velX = -1
+                gato['velX'] = -1
             elif e.key == K_w:
-                velY = -1
+                gato['velY'] = -1
             elif e.key == K_s:
-                velY = 1
+                gato['velY'] = 1
         elif e.type == KEYUP:
             if e.key == K_d:
-                velX = 0
+                gato['velX'] = 0
             elif e.key == K_a:
-                velX = 0
+                gato['velX'] = 0
             elif e.key == K_w:
-                velY = 0
+                gato['velY'] = 0
             elif e.key == K_s:
-                velY = 0
+                gato['velY'] = 0
