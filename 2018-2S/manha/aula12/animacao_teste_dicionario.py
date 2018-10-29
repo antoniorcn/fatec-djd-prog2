e29107d9b62670117414cb207ab931210b86f429
Antonio Carvalho <antoniorcn@hotmail.com>
2018-10-29 11:02:17 -0300

    Manha aula 12 parte 2

--- 2018-2S/manha/aula12/animacao_teste_dicionario.py
+++ 2018-2S/manha/aula12/animacao_teste_dicionario.py
@@ -1,11 +1,11 @@
 import pygame
-from pygame import QUIT
+from pygame import QUIT, KEYUP, KEYDOWN, K_UP, K_DOWN
 
 pygame.init()
 tela = pygame.display.set_mode((800, 600), 0, 32)
 sprite_sheet = pygame.image.load("EpicArmor.png").convert_alpha()
 
-sergio = {  "state": "images_state_up_stop",
+sergio = {  "state": "images_state_down_run",
             "images_state_up_run":[1, 2, 3, 4, 5, 6, 7, 8],
             "images_state_up_stop":[0],
             "images_state_down_run": [19, 20, 21, 22, 23, 24, 25, 26],
@@ -30,28 +30,23 @@ def get_frame( gId ):
     return img
 
 def anima( obj ):
+    global tela
     chave = obj["state"]
     frames = obj[chave]
     obj["frame_index"] += 1
-    if obj["frame_index"] > len(frames):
+    if obj["frame_index"] >= len(frames) - 1:
         obj["frame_index"] = 0
     gId = frames[obj["frame_index"]]
     sur_frame = get_frame(gId)
     tela.blit(sur_frame, (400, 300))
 
-frame_index = 19
 clk = pygame.time.Clock()
-
 while True:
     # Calcular Regras
-    frame_index += 1
-    if frame_index > 26:
-        frame_index = 19
 
     # Pintar
     tela.fill((0, 0, 0))
-    frame = get_frame( frame_index )
-    tela.blit(frame, (400, 300))
+    anima(sergio)
     pygame.display.update()
     clk.tick(10)
 
@@ -59,3 +54,13 @@ while True:
     for e in pygame.event.get():
         if e.type == QUIT:
             exit()
+        elif e.type == KEYDOWN:
+            if e.key == K_UP:
+                sergio["state"] = "images_state_up_run"
+            elif e.key == K_DOWN:
+                sergio["state"] = "images_state_down_run"
+        elif e.type == KEYUP:
+            if e.key == K_UP:
+                sergio["state"] = "images_state_up_stop"
+            elif e.key == K_DOWN:
+                sergio["state"] = "images_state_down_stop"
