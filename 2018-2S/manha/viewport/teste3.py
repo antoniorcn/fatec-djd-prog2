9c3e3f288bdda25151c3084f6290727563543ebb
Antonio Rodrigues Carvalho Neto <antoniorcn@hotmail.com>
2018-10-29 16:29:36 -0300

    Manha aula12 parte 6

--- 2018-2S/manha/viewport/teste3.py
+++ 2018-2S/manha/viewport/teste3.py
@@ -1,79 +1,72 @@
-'''
-Created on 30/10/2014
-
-@author: Aluno
-'''
-
 import json
 import pygame
-
 from tiles import TileSet, Layer
-from pygame.constants import QUIT, KEYDOWN, K_RIGHT, K_LEFT, K_UP, K_DOWN
+from pygame.constants import QUIT, KEYDOWN, KEYUP, K_RIGHT, K_LEFT, K_UP, K_DOWN
 from pygame.rect import Rect
 
+VIEW_PORT_WIDTH = 400
+VIEW_PORT_HEIGHT = 400
+
 filename = "mapa.json"
+f = open(filename)
+jobj = json.load(f)
 
-f = open( filename )
-
-jobj = json.load( f )
-
-ts = TileSet( jobj["tilesets"][0] )
-
-print(jobj["tilesets"][0]["image"])
-
+ts = TileSet(jobj["tilesets"][0])
 pygame.init()
-screen = pygame.display.set_mode( (640, 480), 0, 32)
+screen = pygame.display.set_mode((800, 600), 0, 32)
 
-layer = Layer( jobj["layers"][0] )
-layer.prepare( ts )
-print(layer.width, layer.height)
+layer = Layer(jobj["layers"][0])
+layer.prepare(ts)
 x = 0
 y = 0
-velX = 20
-velY = 20
+velX = 0
+velY = 0
 clk = pygame.time.Clock()
-
-cooldown = 1 * 1000
-
+cooldown = 1 * 1
 inicio = pygame.time.get_ticks()
-
-cenario = Rect( (0, 0), (20 * 32, 20 * 32) )
+cenario = Rect((0, 0), (20 * 32, 20 * 32))
 while (True):
-    screen.fill( (0, 0, 0) )
-    
-    r = Rect ((x, y), (200, 200))   
-    
-    img = layer.getFrameRect( r )
-    
-    screen.blit(img, (10, 10) )
-            
+
+    #Pintar
+    screen.fill((0, 0, 0))
+    r = Rect((x, y), (VIEW_PORT_WIDTH, VIEW_PORT_HEIGHT))
+    img = layer.get_frame_rect(r)
+    screen.blit(img, (10, 10))
     pygame.display.update()
-      
     
-    clk.tick( 60 )
+    clk.tick(120)
+
+    #Capturar Eventos
     for e in pygame.event.get():
-        
-        if (e.type == QUIT):
+        if e.type == QUIT:
             exit()
-            
-        if (e.type == KEYDOWN):
-            if (e.key == K_RIGHT):
-                velX = 1
-            if (e.key == K_LEFT):
-                velX = -1
-            if (e.key == K_UP):
-                velY = -1
-            if (e.key == K_DOWN):
-                velY = 1
-                
-        
+        if e.type == KEYDOWN:
+            if e.key == K_RIGHT:
+                velX = 10
+            if e.key == K_LEFT:
+                velX = -10
+            if e.key == K_UP:
+                velY = -10
+            if e.key == K_DOWN:
+                velY = 10
+        if e.type == KEYUP:
+            if e.key == K_RIGHT:
+                velX = 0
+            if e.key == K_LEFT:
+                velX = 0
+            if e.key == K_UP:
+                velY = 0
+            if e.key == K_DOWN:
+                velY = 0
+
     agora = pygame.time.get_ticks()
-            
-    if (agora > (inicio + cooldown)):
+
+    if agora > (inicio + cooldown):
         inicio = agora
-        rDesejavel = Rect ( (x + velX, y + velY), (201, 201) )
+        rDesejavel = Rect((x + velX, y + velY),
+                          (VIEW_PORT_WIDTH + 1, VIEW_PORT_HEIGHT + 1))
            
-        if ( cenario.contains( rDesejavel ) ):
+        if cenario.contains(rDesejavel):
             x += velX
             y += velY
     
--- 2018-2S/manha/viewport/tiles.py
+++ 2018-2S/manha/viewport/tiles.py
@@ -11,20 +11,20 @@ class Conversao(object):
     def __init__(self, ratio):
         self.ratio = ratio
         
-    def getWorldToPixels(self, pos):
+    def get_world_to_pixels(self, pos):
         px = pos[0] * self.ratio[0]
         py = pos[1] * self.ratio[1]
-        return (px, py)
-    
-    def getPixelsToWorld(self, pos):
-        wx = pos[0] / self.ratio[0]
-        wy = pos[1] / self.ratio[1]
-        return (wx, wy)
+        return px, py
 
-    def getPixelsToWorldGap(self, pos):
+    def get_pixels_to_world(self, pos):
+        wx = int(pos[0] / self.ratio[0])
+        wy = int(pos[1] / self.ratio[1])
+        return wx, wy
+
+    def get_pixels_to_world_gap(self, pos):
         gx = pos[0] % self.ratio[0]
         gy = pos[1] % self.ratio[1]
-        return (gx, gy)
+        return gx, gy
 
 
 class TileSet(object):
@@ -38,28 +38,27 @@ class TileSet(object):
         self.margin = jsonObj['margin']
         self.spacing = jsonObj['spacing']
         self.tileoffset = jsonObj['tileoffset']
-        self.columns = self.image_width / self.tile_width
-        self.lines = self.image_height / self.tile_height
+        self.columns = int(self.image_width / self.tile_width)
+        self.lines = int(self.image_height / self.tile_height)
         self.image = pygame.image.load(jsonObj['image'])
         self.lastgid = self.firstgid - 1 + (self.columns * self.lines)
-        print("TileSet : ", self.name, " carregado...")
 
     def getTileByGId(self, gId):
         currentFrame = (gId - self.firstgid)
         column = currentFrame % self.columns
-        line = currentFrame / self.columns
-        x = self.margin + ( (self.tile_width + self.tileoffset["x"] ) * column)
-        y = self.spacing + ( (self.tile_height  + self.tileoffset["y"] ) * line )
+        line = int(currentFrame / self.columns)
+        x = self.margin + ((self.tile_width + self.tileoffset["x"]) * column)
+        y = self.spacing + ((self.tile_height + self.tileoffset["y"]) * line)
         w = self.tile_width
         h = self.tile_height
-        r = Rect( (x, y), (w, h) )
-        image = self.image.subsurface( r )
-        brick = Brick( self, image, gId )
+        r = Rect((x, y), (w, h))
+        image = self.image.subsurface(r)
+        brick = Brick(self, image, gId)
         return brick
       
     
 
-class Brick():
+class Brick(object):
     def __init__(self, tileset, imagem, gId):
         self.tileset = tileset
         self.image = imagem
@@ -69,7 +68,7 @@ class Brick():
         self.rect = Rect( (0,0), (imagem.get_width(), imagem.get_height()))  
         
         
-class Layer( object ):
+class Layer(object):
     def __init__(self, json):
         self.name = json['name']
         self.type = json['type']
@@ -79,39 +78,38 @@ class Layer( object ):
         self.opacity = json['opacity']
         self.__data = json['data']
         self.internal_structure = []
-        self.conversion = Conversao( (32, 32) )
-        
+        self.conversion = {}
         
     def prepare(self, tile_set):
+        self.conversion = Conversao((tile_set.tile_width, tile_set.tile_height))
         self.internal_structure = []
         for l in range(self.height):
             celulas = []
             for c in range(self.width):
                 gid = self.__data[(l * self.width) + c]
-                brk = Brick( tile_set, Surface( (32, 32), 0, 32), 0) 
-                if (gid > 0):               
+                brk = Brick(tile_set,
+                        Surface((tile_set.tile_width, tile_set.tile_height), 0, 32), 0)
+                if gid > 0:
                     brk = tile_set.getTileByGId(gid)
-                celulas.append( brk )
+                celulas.append(brk)
             self.internal_structure.append(celulas)
 
 
-    def getFrame(self, c1, l1, c2, l2):
+    def get_frame(self, c1, l1, c2, l2):
         cols = abs(c2 - c1)
         rows = abs(l2 - l1)        
-        img = pygame.Surface( self.conversion.getWorldToPixels( (cols, rows) ), 0, 32 )
-        for l in range( rows ):
-            for c in range ( cols ):
+        img = pygame.Surface(self.conversion.get_world_to_pixels((cols, rows)), 0, 32)
+        for l in range(rows):
+            for c in range(cols):
                 b = self.internal_structure[l + l1][c + c1]
-                pos = self.conversion.getWorldToPixels( (c, l) )
-                
-                img.blit( b.image, pos )
+                pos = self.conversion.get_world_to_pixels((c, l))
+                img.blit(b.image, pos)
         return img
 
-                
-    def getFrameRect(self, r):
-        (c1, l1) = self.conversion.getPixelsToWorld( r.topleft )
-        (c2, l2) = self.conversion.getPixelsToWorld( r.bottomright )
-        img = self.getFrame(c1, l1, c2 + 1, l2 + 1)
-        gapTop = self.conversion.getPixelsToWorldGap( r.topleft )
-        imgFinal = img.subsurface( Rect( gapTop, r.size ) )
+    def get_frame_rect(self, r):
+        (c1, l1) = self.conversion.get_pixels_to_world(r.topleft)
+        (c2, l2) = self.conversion.get_pixels_to_world(r.bottomright)
+        img = self.get_frame(c1, l1, c2 + 1, l2 + 1)
+        gapTop = self.conversion.get_pixels_to_world_gap(r.topleft)
+        imgFinal = img.subsurface(Rect(gapTop, r.size))
         return imgFinal
